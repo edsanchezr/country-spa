b1ef2f2ac839ce0ca64fb8a5c037101eecfc337c
Eduardo Sanchez <edsanchezr.91@gmail.com>
2023-08-21 18:51:49 -0600

    fin seccion 15

--- src/app/heroes/pipes/hero-image.pipe.ts
+++ src/app/heroes/pipes/hero-image.pipe.ts
@@ -11,6 +11,8 @@ export class HeroImagePipe implements PipeTransform {
       return 'assets/no-image.png';
     }
 
+    if (hero.alt_img) return hero.alt_img;
+
     return `assets/heroes/${hero.id}.jpg`
   }
 
